e5374a458a3a7b8093d239efb7829820c1694dc9
Krishna Srinivas Madhira <krishnasrinivas2409@gmail.com>
2025-03-08 22:34:47 +0530

    Added GUI and Talk Back Feature to Omnitrix

---
+++ OMNITRIX/main.py
@@ -0,0 +1,16 @@
+import threading
+from omnitrix_ai import listen
+from omnitrix_gui import update_pulse
+
+# Run Speech Recognition in a Separate Thread
+def voice_thread():
+    while True:
+        listen(update_pulse)  # Listen for commands & trigger UI
+
+# Start Voice Processing
+threading.Thread(target=voice_thread, daemon=True).start()
+
+# Run Omnitrix GUI (Main Thread)
+import omnitrix_gui  # This will start the GUI loop
+# The GUI will be updated by the voice_thread
+# The voice_thread will be triggered by the GUI
