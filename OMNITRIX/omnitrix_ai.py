731f5ce69c8b2f324014d864eb8c4df11e087bf8
Krishna Srinivas Madhira <krishnasrinivas2409@gmail.com>
2025-03-08 22:14:47 +0530

    Initial Commit + Omnitrix AI : talk back function

---
+++ OMNITRIX/omnitrix_ai.py
@@ -0,0 +1,23 @@
+import pyttsx3
+import speech_recognition as sr
+
+engine =pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def listen():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening . . .")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+    try:
+        command = recognizer.recognize_google(audio).lower()
+        print(f"You said: {command}")
+        return command
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+    except sr.RequestError:
+        return "Could not request results"
